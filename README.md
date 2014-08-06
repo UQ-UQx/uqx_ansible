86b095b296469d8e0f2458edb9dccdcc7132c747
Andrew Dekker <uqadekke@uq.edu.au>
2014-08-06 17:00:03 +1000

    Nectar VM and readme

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-uqx_ansible
+UQx Configuration Script
 ===========
 
 Ansible scripts for UQx
+
+Usage
+---------------------
+
+Launch NECTAR VM (optional)
+---------------------
+ - Adapted from https://github.com/eResearchSA/nectar-ansible
+ - Login to the Nectar Dashboard (https://dashboard.rc.nectar.org.au)
+ - Choose the correct [PROJECT_NAME]
+ - Create a keypair in "Access and Security" and note the [KEY_NAME]
+ - Go to "Settings" (top right)
+ - Click "Reset Password"
+ - Copy down the new [GENERATED_PASSWORD]
+ - Go to "Access and Security"
+ - Go to the "API Access"
+ - Click "Download OpenStack RC File"
+ - Run the script
+ ```bash
+ source ./[PROJECT_NAME]-openrc.sh
+ ```
+ - Enter the API [GENERATED_PASSWORD]
+ - Run the nectar script
+ ```bash
+ python startvm.py -k [KEY_NAME]
+ ```
+
+Configure VM with Ansible
+---------------------
--- nectar/startvm.py
+++ nectar/startvm.py
@@ -9,18 +9,20 @@ import sys, os, time, socket, argparse, random, urllib2
 
 from novaclient.v1_1 import client
 
-default_image = urllib2.urlopen("https://swift.rc.nectar.org.au:8888/v1/AUTH_6f3b184bcf194672a55d258620e732db/deploy/nectar-zfs-image").read().strip()
+#default_image = urllib2.urlopen("https://swift.rc.nectar.org.au:8888/v1/AUTH_6f3b184bcf194672a55d258620e732db/deploy/nectar-zfs-image").read().strip()
+default_image = '198869c6-d8f5-4972-8085-e2a5dc1e139d'
 default_setup_script = None
 default_security_group = "default"
 default_auth_url = "https://keystone.rc.nectar.org.au:5000/v2.0/"
 default_zone = None
+default_size = 'm1.large'
 
 def progress(s):
   sys.stdout.write(s)
   sys.stdout.flush()
 
 def main(image, zone, auth_url, size, key, security_group, setup_script):
-  name = random.sample(open("genus.txt").read().split(), 1)[0]
+  name = random.sample(open("vmnames.txt").read().split(), 1)[0]
   user_data = None
   if setup_script is not None:
     user_data = open(setup_script).read()
@@ -41,7 +43,7 @@ if __name__ == "__main__":
     description = "Fire up a VM on NeCTAR (default image: " + default_image + ")",
     epilog = "Required OpenStack environment variables: OS_USERNAME, OS_PASSWORD, OS_TENANT_NAME.")
   parser.add_argument("-z", "--zone", help = "Zone, e.g. melbourne (default none)", default = default_zone)
-  parser.add_argument("-s", "--size", help = "VM flavour/size, e.g. m1.small", required = True)
+  parser.add_argument("-s", "--size", help = "VM flavour/size, e.g. m1.small", default = default_size)
   parser.add_argument("-i", "--image", help = "Image (default " + default_image + ")", default = default_image)
   parser.add_argument("-k", "--key", help = "Key name", required = True)
   parser.add_argument("-a", "--auth-url", help = "OpenStack Auth URL (default " + default_auth_url + ")", default = default_auth_url)
